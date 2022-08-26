0a5155782bc2a74005868a20bee681eb27f9bdae
Aleksandr232 <aleksrowing232@gmail.com>
2022-08-26 12:05:32 +0300

    router

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
+
 export default function Index() {
   return (
     <>
       <h1>Hello next.js</h1>
+      <p><Link href={'/about'} ><a>About</a></Link></p>
+      <p><Link href={'/posts'} ><a>Post</a></Link></p>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit
         aperiam ipsa expedita. Cupiditate quaerat animi harum nemo, nam
