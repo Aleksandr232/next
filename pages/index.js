1ee915340c81e5b022fc33cbc7a7369c6eddaa4b
Aleksandr232 <aleksrowing232@gmail.com>
2022-08-26 11:42:27 +0300

    index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-export default function Index(){
-    return(
-        <h1>Hello next.js</h1>
-    )
+export default function Index() {
+  return (
+    <>
+      <h1>Hello next.js</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit
+        aperiam ipsa expedita. Cupiditate quaerat animi harum nemo, nam
+        exercitationem distinctio dolores dolore laboriosam nulla cumque
+        repellat, ab porro odit? Sequi!
+      </p>
+    </>
+  );
 }
