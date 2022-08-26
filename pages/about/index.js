af6e7afbdd4b03ab029c25b6bd08ea212d37583b
Aleksandr232 <aleksrowing232@gmail.com>
2022-08-26 12:14:35 +0300

    index

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,5 +1,17 @@
+import  Router  from "next/router"
+
 export default function About(){
+
+    const linkClickHandler=()=>{
+        Router.push('/')
+    }
     return(
-        <h1>About Page</h1>
+        <>
+            <h1>About Page</h1>
+
+            <button onClick={linkClickHandler}>Go back to home </button>
+            <button onClick={()=>Router.push('/posts')}>Go  to posts inline </button>
+        </>
+        
     )
 }
