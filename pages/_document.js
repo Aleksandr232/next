f914ec7128b7a834410105f3d40638b0c267402a
Aleksandr232 <aleksrowing232@gmail.com>
2022-08-26 16:54:55 +0300

    document

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+            <link href="https: //fonts.googleapis.com/css2?family= Roboto:ital, wght @1,300 & display=swap" rel="stylesheet"/>
+        </Head>
         <body>
           <Main />
           <NextScript />
