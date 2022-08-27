56ee9f214cbf8e08e24f1ffcfb737757f3af1642
Aleksandr232 <aleksrowing232@gmail.com>
2022-08-27 12:44:15 +0300

    app

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,11 @@ export default function MyApp({Component, pageProps}){
     return(
         <>
             <Component {...pageProps}/>
+            <style jsx global>{`
+                body{
+                    font-family: 'Roboto', sans-serif;
+                }
+            `}</style>
         </>
     )
 }
