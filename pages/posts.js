f3e26790c0a21e26c0ca874b3b2cc35ddc4a0454
Aleksandr232 <aleksrowing232@gmail.com>
2022-08-27 13:34:47 +0300

    post

--- pages/posts.js
+++ pages/posts.js
@@ -1,6 +1,20 @@
+import {useState, useEffect} from "react"
 import { MainLayout } from "./components/MainLayout"
 
+
+
 export default function Posts(){
+    const [post, setPost]=useState([])
+
+    useEffect(()=>{
+        async function load(){
+            const response = await fetch('http://localhost:4200/posts')
+            const json = await response.json()
+            setPost(json)
+        }
+        load()
+    },[])
+
     return (
     <MainLayout title='Post'>
         <h1>Posts Page</h1>
