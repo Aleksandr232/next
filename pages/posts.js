fd545c96cdc378b0758be8c3ae78ec2cd4d493e7
Aleksandr232 <aleksrowing232@gmail.com>
2022-08-27 21:48:04 +0300

    posts

--- pages/posts.js
+++ pages/posts.js
@@ -22,7 +22,9 @@ export default function Posts({posts}){
         <ul>
             {posts.map(post=>(
                 <li key={post.id}>
-                    <Link href={`/post/${post.id}`}><a>{post.title}</a></Link>
+                    <Link href={`/post/[id]`} as={`/post/${post.id}`}>
+                        <a>{post.title}</a>
+                    </Link>
                 </li>
         ))}</ul>
     </MainLayout>
