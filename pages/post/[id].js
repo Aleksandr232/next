07e485e1f565a7ae7af740fbaad570bcb0afc50b
Aleksandr232 <aleksrowing232@gmail.com>
2022-08-27 22:00:35 +0300

    post id

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,12 +1,26 @@
 import { useRouter } from "next/router"
 import { MainLayout } from "../components/MainLayout"
+import Link from "next/link"
 
-export default function Post(){
-    const router = useRouter()
+export default function Post({post}){
+   
     
     return (
         <MainLayout title='Post'>
-            <h1>Post: {router.query.id}</h1>
+            <h1>{post.title}</h1>
+            <hr />
+            <p>{post.text}</p>
+            <Link href={'/posts'}><a>Back to all posts</a></Link>
         </MainLayout>
     )
+}
+
+Post.getInitialProps = async(ctx)=>{
+    const response = await fetch(`http://localhost:4200/posts/${ctx.query.id}`)
+    const post = await response.json()
+
+
+    return {
+        post
+    }
 }
