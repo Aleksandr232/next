dd688d5807c45cebe06ef195137772c8eadafd9a
Aleksandr232 <aleksrowing232@gmail.com>
2022-08-29 13:04:49 +0300

    app

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -8,56 +8,56 @@ export default function Post({post: serverPost}){
    const [post, setPost] = useState(serverPost)
    const router = useRouter()
 
-   useEffect(()=>{
-    async function load(){
-        const response = await fetch(`http://localhost:4200/posts/${router.query.id}`)
-        const data = await response.json()
-        setPost(data)
+   useEffect(() => {
+    async function load() {
+      const response = await fetch(`http://localhost:4200/posts/${router.query.id}`)
+      const data = await response.json()
+      setPost(data)
     }
 
-    if(!serverPost){
-        load()
+    if (!serverPost) {
+      load()
     }
-   },[])
+  }, [])
 
-   if(!post){
+  if (!post) {
     return <MainLayout>
-        <p>Loading...</p>
+      <p>Loading ...</p>
     </MainLayout>
-   }
-    
-    return (
-        <MainLayout title='Post'>
-            <h1>{post.title}</h1>
-            <hr />
-            <p>{post.text}</p>
-            <Link href={'/posts'}><a>Back to all posts</a></Link>
-        </MainLayout>
-    )
+  }
+
+  return(
+    <MainLayout>
+      <h1>{post.title}</h1>
+      <hr />
+      <p>{post.body}</p>
+      <Link href={'/posts'}><a>Back to all posts</a></Link>
+    </MainLayout>
+  )
 }
 
-Post.getInitialProps = async ({req})=>{
-    if(!req){
-        return {post: null}
-    }
+
+
+
+Post.getInitialProps = async ({ query, req }) => {
+  if (!req) {
+    return {post: null}
+  }
 
     const response = await fetch(`http://localhost:4200/posts/${query.id}`)
     const post = await response.json()
 
-
-    return {
-        post
-    }
+  return {
+    post
+  }
 }
 
-/* export async function getServerSideProps({query, req}){
-    if(!req){
-        return {post: null}
-    }
-
-    const response = await fetch(`http://localhost:4200/posts/${query.id}`)
-    const post = await response.json()
-
-
-    return {props: {post}}
-} */
+// export async function getServerSideProps({ query, req }) {
+//   // if (!req) {
+//   //   return {post: null}
+//   // }
+//   const response = await fetch(`http://localhost:4200/posts/${query.id}`)
+//   const post = await response.json()
+//
+//   return {props: {post}}
+// }
