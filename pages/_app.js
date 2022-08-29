4afebb85a3f67b22a123e09415c01e12c1f78fe1
Aleksandr232 <aleksrowing232@gmail.com>
2022-08-29 12:48:20 +0300

    app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import NProgress from "next-nprogress/component";
+
+
+
 export default function MyApp({Component, pageProps}){
     return(
         <>
-            <Component {...pageProps}/>
+        <NProgress
+            color="#29d"
+            options={{ trickleSpeed: 50 }}
+            showAfterMs={300}
+            spinner
+            />
+        <Component {...pageProps}/>
             <style jsx global>{`
                 body{
                     font-family: 'Roboto', sans-serif;
